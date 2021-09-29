17b323b4fa45816f67e4ce3acc20a3815eb649f9
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-29 14:45:45 +0100

    2 tests, can raise salary

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,2 +1,22 @@
+import management.Manager;
+import org.junit.Test;
+import staff.Employee;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class EmployeeTest {
+
+    Employee employee;
+
+    @Before
+    public void before() {
+        employee = new Manager("Accounting", "John Smith", "123456789AB", 12000.0);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        assertEquals(12100.0, employee.raiseSalary(100.0), 0.01);
+    }
 }
